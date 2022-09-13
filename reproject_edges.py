f1517cdeb6fb05bd8ac942e979e5ae33821127bd
SpacialTreeHPG <savannahgramze@ufl.edu>
2022-09-13 12:24:47 -0400

    update 9.13.2022

---
+++ reproject_edges.py
@@ -0,0 +1,17 @@
+from spectral_cube import SpectralCube
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read in a spectral cube
+cubename = '/orange/adamginsburg/cmz/g5/G5/sum/newcombination/g5.H2CO.spw23_test.fits' # Example data
+cube = SpectralCube.read(cubename)
+
+mom0 = cube[:1]#cube.moment0()
+repro_mom0 = mom0.reproject(mom0.header)#mom0.reproject(mom0.header)
+
+mom0_mask = np.isfinite(mom0)
+repro_mom0_mask = np.isfinite(repro_mom0)
+
+edges = np.logical_xor(mom0_mask, repro_mom0_mask)
+
+plt.imshow(edges[0])
